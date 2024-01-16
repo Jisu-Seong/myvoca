6954fad6c53205661bcd9ffa374f06764b994b78
Jisu-Seong <sjangela0615@gmail.com>
2024-01-16 13:03:58 +0900

    upload files

---
+++ myvoca_backend/vocaapi/src/test/java/com/example/vocaapi/VocaapiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.vocaapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VocaapiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
